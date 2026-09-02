051a1eb96b266639587f961638ded7cd41c47c9a
tperes18 <tperesfr@gmail.com>
2026-09-02 14:04:43 -0300

    Atualizar ALERTA

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const diaDaEntrega = new Date('October 23, 2026 00:00:00').getTime();
 const aviso = document.querySelector('#aviso');
 const subtitulo = document.querySelector('#subtitulo');
+const gifAlerta = document.querySelector('#gif');
 
 function atualizarDiasRestantes() {
     const hoje = new Date().getTime();
@@ -9,28 +10,44 @@ function atualizarDiasRestantes() {
 
     if (tempoRestante <= 0) {
         aviso.textContent = 'O dia chegou.';
-        return;
+        aviso.style.fontSize = '7vw';
+        return 0;
     }
 
     const milisegundosEmUmDia = 1000 * 60 * 60 * 24;
     const diasRestantes = Math.ceil(tempoRestante / milisegundosEmUmDia);
 
-    
+    if (diasRestantes === 1) {
+        aviso.innerHTML = `Falta <span id="vermelho">${diasRestantes}</span> dia`;
+        subtitulo.innerHTML = `Para a entrega das 20 funcionalidades de projeto de software`;
+        return diasRestantes;
+    }
 
     aviso.innerHTML = `Faltam <span id="vermelho">${diasRestantes}</span> dias`;
-    subtitulo.innerHTML = `Para a entrega das 20 funcionalidades de projeto de software`
+    subtitulo.innerHTML = `Para a entrega das 20 funcionalidades de projeto de software`;
+    return diasRestantes;
 }
 
-function definirTamanhoAlerta(diasRestantes) {
-    // max: 100vw
-    // min (51 dias ou menos): 20vw
+// diasRestantes -> quantos dias faltam para o fim
+// prazoInicial -> quantos dias tínhamos quando soubemos do prazo
+// tamanhoMinimo -> tamanhoMinimo do alerta
+// tamanhoMaximo -> tamanhoMaximo do alerta
+function definirTamanhoAlerta(elementoAlerta, diasRestantes, prazoInicial = 51, tamanhoMinimo = 20, tamanhoMaximo = 80) {
+    // DEFINIR VALOR DO TAMANHO
+    if (diasRestantes > prazoInicial) {
+        diasRestantes = prazoInicial;
+    } else if (diasRestantes < 0) {
+        diasRestantes = 0;
+    }
+
+    const porcentagemConcluida = 1 - (diasRestantes / prazoInicial);
+
+    const tamanhoAlerta = (((tamanhoMaximo - tamanhoMinimo) * porcentagemConcluida) + tamanhoMinimo).toFixed(1);
+
+    // APLICAR TAMANHO NO ELEMENTO
     
-    const tamanhoMax = 100;
-    const tamanhoMin = 20;
-
-    // TODO: definir equação para calcular tamanhoAlerta;
-
-    return tamanhoAlerta;
+    elementoAlerta.style.height = `${tamanhoAlerta}vw`;
 }
 
-atualizarDiasRestantes();
+const diasRestantes = atualizarDiasRestantes();
+definirTamanhoAlerta(gifAlerta, diasRestantes);
